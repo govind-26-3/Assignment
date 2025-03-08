82bf6a34b90b93d82efa9311ed16eab809f14342
Govind Lohar <142050985+govind-26-3@users.noreply.github.com>
2025-03-09 02:29:47 +0530

    SQL assignment 1 and 2

--- SQLQuery.sql
+++ SQLQuery.sql
@@ -93,23 +93,25 @@ VALUES
 
 
 select * from Books
--- Update the price of a book titled "SQL Mastery" by increasing it by 
---10%
+-- Update the price of a book titled "SQL Mastery" by increasing it by 10%
 
 Update Books
 set Price=Price + (Price*.1)
 where Title='SQL Mastery'
 
+--3. Delete a customer who has not placed any orders.
 
 delete from Customers  
 where CustomerID in (SELECT c.CustomerID FROM Customers c
     LEFT JOIN Orders o ON c.CustomerID = o.CustomerId
     WHERE o.OrderID IS NULL);
 
+--1. Retrieve all books with a price greater than 2000. 
 
 select * from Books where Price>2000
 
 
+--2. Find the total number of books available. 
 
 select count(BookID) NumberOfBooks 
 from Books;
@@ -119,6 +121,7 @@ from Books;
 select * from Books 
 where PublishedYear between  2015 and 2023;
 
+--4. Find all customers who have placed at least one order. 
 
 select * from Customers where customerID in 
 (SELECT o.CustomerID FROM Customers c
@@ -126,33 +129,51 @@ select * from Customers where customerID in
     group by o.CustomerId
 	having count(*)>=1)
 
+--5. Retrieve books where the title contains the word "SQL". 
+
 select * from Books where title like '%SQL%';
 
+--6. Find the most expensive book in the store. 
 select * from books where price=(select Max(Price) from books)
 
+--7. Retrieve customers whose name starts with "A" or "J". 
 select * from Customers where Name like 'A%'  Or Name like 'J%'
 
+--8. Calculate the total revenue from all orders. 
 select sum(TotalAmount) as Revenue from Orders;
+
+--1. Retrieve all book titles along with their respective author names. 
 
 select a.Name Author_Name ,b.Title Book_title 
 from Authors a
 join Books b
 on a.AuthorID = b.AuthorId
 
+--2. List all customers and their orders (if any). 
+
+
 SELECT c.CustomerID CustomerID,c.Email  Email, c.PhoneNumber PhoneNumber, 
 c.Name Customer_Name, o.OrderID OrderID, o.OrderDate OrderDate, o.TotalAmount TotalAmount
 FROM Customers c
 LEFT JOIN Orders o ON c.CustomerID = o.CustomerID;
+
+
+--3. Find all books that have never been ordered. 
+
 
 select b.Title Title, b.AuthorId AuthorId,b.price price,b.PublishedYear PublishedYear
 from Books b left join OrdersItems o 
 on b.BookId = o.BookId 
 where o.bookId is null
 
+--4. Retrieve the total number of orders placed by each customer. 
+
 select CustomerID, count(orderId) as total_orders 
 from orders  
 group by customerID
 
+
+--5. Find the books ordered along with the quantity for each order. 
 
 
 SELECT    o.OrderID  orderID,   oi.BookID BookId,   b.Title Title,   oi.Quantity Quantity
@@ -160,6 +181,7 @@ FROM Orders o
 JOIN OrderItems oi ON o.OrderID = oi.OrderID
 JOIN Books b ON oi.BookID = b.BookID;
 
+--6. Display all customers, even those who haven’t placed any orders. 
 
 
 select c.CustomerID, Name, Email, PhoneNumber
@@ -167,7 +189,7 @@ from Customers c
 left join orders o
 on c.CustomerId=o.CustomerId
 
-
+--7. Find authors who have not written any books 
 
 select a.Name 
 from Authors a
---
+++ SubqueryAndStoredProcedures.sql
@@ -0,0 +1,77 @@
+--Find the customer(s) who placed the first order 
+
+SELECT * 
+FROM Customers
+WHERE CustomerID = (
+    SELECT TOP 1 CustomerID
+    FROM Orders
+    ORDER BY OrderDate DESC
+)
+
+--2. Find the customer(s) who placed the most orders. 
+
+SELECT  c.CustomerID, c.Name, c.Email,  COUNT(o.OrderID) AS OrderCount
+FROM Customers c
+JOIN Orders o ON c.CustomerID = o.CustomerID
+GROUP BY c.CustomerID, c.Name, c.Email
+having  count(o.OrderID) = (
+	select Max(OrderCount) from(
+	select Count(CustomerId) OrderCount from orders
+	group by CustomerId
+	) orders
+) 
+
+--3. Find customers who have not placed any orders
+
+select * from Customers
+where CustomerID not in (select CustomerID from Orders)
+
+
+--4. Retrieve all books cheaper than the most expensive book written by
+select * from books 
+where price < (select Max(Price) from Books)
+
+--List all customers whose total spending is greater than the average spending of all customers
+
+select * from Customers 
+where CustomerID in 
+	(select CustomerID from Orders
+	where TotalAmount >
+		(select Avg(Total_Sum) from(
+			select sum(TotalAmount) Total_Sum from Orders
+			group By CustomerId) AvgAmount
+		)
+	)
+
+--1. Write a stored procedure that accepts a CustomerID and returns all orders placed by that customer 
+Create Procedure GetAllOrderById
+ @customerId int 
+ As
+Begin
+Select * from Orders where CustomerId=@customerId
+end
+
+exec GetAllOrderById 2
+
+
+--2. Create a procedure that accepts MinPrice and MaxPrice as parameters and returns all books within that range
+
+Create Proc GetBooks
+@minPrice int ,
+@maxPrice int
+as
+Begin
+select * from Books where Price between @minPrice and  @maxPrice;
+end
+
+exec GetBooks 300,1000
+
+--.Create a view named AvailableBooks that shows only books that are in stock, including BookID, Title, AuthorID, Price, and PublishedYear
+
+create view AvailableBooks
+as
+select BookID, Title, AuthorID, Price, PublishedYear
+from Books
+
+
+select * from AvailableBooks
